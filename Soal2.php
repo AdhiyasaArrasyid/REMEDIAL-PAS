e41736c91c58ba7bc374fa358c16bb64ed2f61e4
Adhiyasa <110332038+AdhiyasaArrasyid@users.noreply.github.com>
2022-12-14 21:19:50 +0700

    first commit

--- Soal2.php
+++ Soal2.php
@@ -4,13 +4,13 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+      <title>Data Siswa</title>
 </head>
 <body>
       <Form action="soal2-2.php" method="GET">
       <table border="1">
         <tr>
-            <th colour = "red "colspan="1000">Pendaftaran Siswa Berprestasi</th>
+             <th font color = "red " colspan="1000">Pendaftaran Siswa Berprestasi </th>
         </tr>
         <tr>
             <td>
